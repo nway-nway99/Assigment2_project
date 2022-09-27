94a1ffc54bc36bdc48d4cc562aa41bf6f0127089
nway-nway99 <113897244+nway-nway99@users.noreply.github.com>
2022-09-27 12:16:46 +0630

    Add files via upload

---
+++ assigment2.js
@@ -0,0 +1,11 @@
+const express = require ('express');
+const path = require('path');
+
+const app = express();
+const publicPath=path.join(__dirname,'html_files')
+
+
+app.use(express.static(publicPath));
+
+
+app.listen(5000);
